5a3524940ae3e4a03026c50866a535f60af6dbf4
alinmiron <alin.miron.work@gmail.com>
2014-12-09 11:37:12 +0200

    modified template to include also a background table which will have the style changed dynamically. -added code that is adding dynamically CSS classes for the background table( today, weekend, other days)

--- src/calendar/template/SchedulerHeaderCalendar.js
+++ src/calendar/template/SchedulerHeaderCalendar.js
@@ -13,63 +13,103 @@ Ext.define('Extensible.calendar.template.SchedulerHeaderCalendar', {
         Ext.apply(this, config);
         this.calendars = config.calendars;
         this.events = config.events;
+
         Extensible.calendar.template.SchedulerHeaderCalendar.superclass.constructor.call(this,
-			'<table class="ext-cal-schedulerview-allday ext-cal-hd-days-tbl" cellpadding="0" cellspacing="0" >',//style="height:100%;"
+			'<table class="ext-cal-bg-tbl" cellpadding="0" cellspacing="0">',
                 '<tbody>',
-                    '<tr class="ext-cal-hd-day thead">',
+                    '<tr>',
                         '<tpl for="calendars">',
-                            '<tpl for=".">',
-                                '<tpl if="IsHidden &#61;&#61; 0">',
-                                    '<th style="height:16px;" class="ext-cal-hd-day">{Title}</th>',
-                                '</tpl>',
-                            '</tpl>',
+                            '<td style="height:22px;"></td>',
                         '</tpl>',
                     '</tr>',
                     '<tr>',
-                        '{% var xxindex = 1; %}',
                         '<tpl for="calendars">',
-                            '<tpl exec="values.cindex = xindex;"></tpl>',
-                            '<tpl for=".">',
-                                '<tpl if="IsHidden &#61;&#61; 0">',
-                                 '<td style="height:20px;" id="{[this.id]}{[values.cindex-1]}-wk--0" class="ext-cal-dtitle {[xxindex==1 ? " ext-cal-dtitle-first" : ""]}">',
-                                    '<div id="{[this.id]}{[values.cindex-1]}-empty-{[xcount-1]}-day-{[Ext.Date.format(this.viewStart,\'Ymd\')]}">',
-                                        '{[xxindex==1 ? Ext.Date.format(this.viewStart,\'M j, Y\'):"&nbsp;"]}',
-                                    '</div>',
-                                 '</td>',
-                        '{% xxindex++; %}',
-                                '</tpl>',
+                            '<tpl if="IsHidden &#61;&#61; 0">',
+                                '<td id="{[this.id]}{[xindex-1]}-week-hd-day-0" class="{[this.cellCls]}">',
+                                 '&#160;',
+                                '</td>',
                             '</tpl>',
                         '</tpl>',
-                    '</tr>',
-                    '<tr>',
-                    '<tpl for="calendars">',
-                        '<tpl if="IsHidden &#61;&#61; 0">',
-                            '<td id="{[this.id]}{[xindex-1]}-wk-0">',
-                                    '<table class="ext-cal-evt-tbl ext-cal-hd-day ext-cal-dayview" cellpadding="0" cellspacing="0" style="height:100%;padding:0px!important;border-top:1px solid #c2d3fd;">',
-                                    '<tpl for=".">',
-                                        '<tpl if="eventscount  &#61;&#61; 0">',
-                                            '<tr style="height:100%;">',
-                                                '<td class="schedulerview-empty-cell" id="{[this.id]}{[xindex-1]}-empty-{[xcount-1]}-day-{[Ext.Date.format(this.viewStart,\'Ymd\')]}">&nbsp;</td>',
-                                            '</tr>',
-                                        '<tpl else>',
-                                            '<tr style="display:none;">',
-                                            '</tr>',
-                                        '</tpl>',
-                                     '</tpl>',
-                                '</table>',
-                            '</td>',
-                        '</tpl>',
-                     '</tpl>',
                     '</tr>',
                 '</tbody>',
-            '</table>'
-        );
+            '</table>',
+            '<table class="ext-cal-schedulerview-allday ext-cal-hd-days-tbl" cellpadding="0" cellspacing="0" >',
+                    '<tbody>',
+                        '<tr class="ext-cal-hd-day thead">',
+                            '<tpl for="calendars">',
+                                '<tpl for=".">',
+                                    '<tpl if="IsHidden &#61;&#61; 0">',
+                                        '<th style="height:16px;" class="ext-cal-hd-day">{Title}</th>',
+                                    '</tpl>',
+                                '</tpl>',
+                            '</tpl>',
+                        '</tr>',
+                        '<tr>',
+                            '{% var xxindex = 1; %}',
+                            '<tpl for="calendars">',
+                                   //'<td></td>',
+                                '<tpl exec="values.cindex = xindex;"></tpl>',
+                                '<tpl for=".">',
+                                    '<tpl if="IsHidden &#61;&#61; 0">',
+                                     '<td style="height:20px;" id="{[this.id]}{[values.cindex-1]}-wk--0" class="{[this.titleCls]}">',
+                                        '<div id="{[this.id]}{[values.cindex-1]}-empty-{[xcount-1]}-day-{[Ext.Date.format(this.viewStart,\'Ymd\')]}">',
+                                            '{[xxindex==1 ? Ext.Date.format(this.viewStart,\'M j, Y\'):"&nbsp;"]}',
+                                        '</div>',
+                                     '</td>',
+                            '{% xxindex++; %}',
+                                    '</tpl>',
+                                '</tpl>',
+                            '</tpl>',
+                        '</tr>',
+                        '<tr>',
+                        '<tpl for="calendars">',
+                            '<tpl if="IsHidden &#61;&#61; 0">',
+                                '<td id="{[this.id]}{[xindex-1]}-wk-0">',
+                                    '<table class="ext-cal-evt-tbl ext-cal-hd-day ext-cal-dayview" cellpadding="0" cellspacing="0" style="height:100%;padding:0px!important;">',
+                                        '<tpl for=".">',
+                                            '<tpl if="eventscount  &#61;&#61; 0">',
+                                                '<tr style="height:100%;">',
+                                                    '<td class="schedulerview-empty-cell" id="{[this.id]}{[xindex-1]}-empty-{[xcount-1]}-day-{[Ext.Date.format(this.viewStart,\'Ymd\')]}">&nbsp;</td>',
+                                                '</tr>',
+                                            '<tpl else>',
+                                                '<tr style="display:none;">',
+                                                '</tr>',
+                                            '</tpl>',
+                                         '</tpl>',
+                                    '</table>',
+                                '</td>',
+                            '</tpl>',
+                         '</tpl>',
+                        '</tr>',
+                    '</tbody>',
+                '</table>'
+            );
     },
 
     applyTemplate: function(o) {
     	
         Ext.apply(this, o);
-        
+
+       var today = Extensible.Date.today(),
+            dt = Ext.Date.clone(this.viewStart),
+            thisMonth = this.startDate.getMonth();
+
+        var isToday = dt.getTime() === today.getTime(),
+        prevMonth = (dt.getMonth() < thisMonth) && this.weekCount === -1,
+        nextMonth = (dt.getMonth() > thisMonth) && this.weekCount === -1,
+        isWeekend = dt.getDay() % 6 === 0;
+
+        this.titleCls = 'ext-cal-dtitle ' + (isToday ? ' ext-cal-dtitle-today' : '') +
+        //(w === 0 ? ' ext-cal-dtitle-first' : '') +
+        (prevMonth ? ' ext-cal-dtitle-prev' : '') +
+        (nextMonth ? ' ext-cal-dtitle-next' : ''),
+
+        this.cellCls = 'ext-cal-day ' + (isToday ? ' ' + this.todayCls : '') +
+        //(d === 0 ? ' ext-cal-day-first' : '') +
+        (prevMonth ? ' ' + this.prevMonthCls : '') +
+        (nextMonth ? ' ' + this.nextMonthCls : '') +
+        (isWeekend && this.weekendCls ? ' ' + this.weekendCls : '');
+
         if (Ext.getVersion('extjs').isLessThan('4.1')) {
             return Extensible.calendar.template.SchedulerHeaderCalendar.superclass.applyTemplate.call(this, {
             	calendars: this.calendars,
