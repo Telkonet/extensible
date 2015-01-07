11b3a3f3050539925db6528f7b5ae8a30e9bc485
alinmiron <alin.miron.work@gmail.com>
2015-01-07 18:43:04 +0200

    Class is inheriting DayHeader class instead of Month. Replaced harcoded text with class variables. Removed obsolete method onDestroy(), superclass is handling this. Removed obsolete method onRecurrenceEditModeSelected(). Refactored renderItems(), the call to WeekEventRenderer.render() method, are receiving viewId parameter built dynamically using CalendarId instead of calendar index. Refactored modifyEvent() method so the obsolete code is removed. Using CalendarId instead of calendar index. Code that is fixing last event row was refactored and now is reduced. Minor changes to the comments.

--- src/calendar/view/SchedulerHeader.js
+++ src/calendar/view/SchedulerHeader.js
@@ -1,6 +1,6 @@
 /**
  * @class Extensible.calendar.view.SchedulerHeader
- * @extends Extensible.calendar.view.Month
+ * @extends Extensible.calendar.view.DayHeader
  * <p>This is the header area container within the day and week views where all-day events are displayed.
  * Normally you should not need to use this class directly -- instead you should use {@link Extensible.calendar.view.Scheduler}
  * which aggregates this class and the {@link Extensible.calendar.view.SchedulerBody} into the single unified view
@@ -9,7 +9,7 @@
  * @param {Object} config The config object
  */
 Ext.define('Extensible.calendar.view.SchedulerHeader', {
-    extend: 'Extensible.calendar.view.Month',
+    extend: 'Extensible.calendar.view.DayHeader',
     alias: 'widget.extensible.schedulerheaderview',
     
     requires: [
@@ -24,7 +24,8 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
     allDayOnly: true,
     monitorResize: false,
     isHeaderView: true,
-
+    eventRowHeight: 16,
+    columnElIdDelimiter: '-calendar-',
     /**
      * @event dayclick
      * Fires after the user clicks within the view container and not on an event element. This is a cancelable event, so
@@ -49,18 +50,10 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
             moveText: this.ddMoveEventText,
             ddGroup: this.ddGroup || this.id + '-SchedulerHeaderDD'
         };
-        // this.dragZone = Ext.create('Extensible.calendar.dd.SchedulerHDragZone', this.el, cfg);
         this.dragZone = Ext.create('Extensible.calendar.dd.DragZone', this.el, cfg);
         this.dropZone = Ext.create('Extensible.calendar.dd.SchedulerHDropZone', this.el, cfg);
     },
 
-    onDestroy: function() {
-        Ext.destroy(this.ddSelector);
-        Ext.destroy(this.dragZone);
-        Ext.destroy(this.dropZone);
-
-        this.callParent(arguments);
-    },
     // private
     afterRender: function() {
 
@@ -71,7 +64,8 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
                 todayText: this.todayText,
                 showTime: this.showTime,
                 calendars: this.calendarStore.data.items,
-                minColumnWidth: this.minColumnWidth
+                minColumnWidth: this.minColumnWidth,
+                columnElIdDelimiter: this.columnElIdDelimiter
             });
         }
         this.tpl.compile();
@@ -108,7 +102,8 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
 
         var max = (this.maxEventsPerDay + 1) || 999;
 
-        maxEvtCount = Ext.Array.max(maxEvtCountPerCalendar)+1;
+
+        maxEvtCount = Ext.Array.max(maxEvtCountPerCalendar);
 
         this.evtMaxCount[weekIndex] = this.evtMaxCount[weekIndex] || 0;
         if (maxEvtCount && this.evtMaxCount[weekIndex] < maxEvtCount) {
@@ -175,7 +170,7 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
                 viewStart: this.viewStart,
                 tpl: this.getEventTemplate(),
                 maxEventsPerDay: this.maxEventsPerDay,
-                viewId: this.id + '-calendar-' + i,
+                viewId: this.id + this.columnElIdDelimiter + calendars[i].data.CalendarId,
                 templateDataFn: Ext.bind(this.getTemplateEventData, this),
                 evtMaxCount: this.evtMaxCount, //adds the empty row
                 weekCount: this.weekCount,
@@ -185,32 +180,21 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
         }
 
         //after renderItems method which uses the default rendering class, we need to be sure that
-        //everything looks good. basically it's about fixing height of the last event row...
-        var eventsDomLabel = this.el.down('.ext-cal-schedulerview-allday').down('tr').next('tr'),
-            eventsDomData = eventsDomLabel.next('tr'),
-            eventRowHeight = 16;
-        var currentDate =this.viewStart;
-        if (eventsDomData !== null) {
-            eventsDomData.select('td > table').each(function(el) {
+        //everything looks good. basically it's about fixing height of the last event cell(row)...
+        var eventsDOMContainers = this.el.down('.ext-cal-schedulerview-allday').select('table'),
+            me = this;
+            eventsDOMContainers.each(function(el) {
+
             var last_event_cell = el.select('td:last');
-                if (last_event_cell !== undefined) {
-                    last_event_cell = last_event_cell.elements[0];
-                    if (last_event_cell !== undefined && last_event_cell.hasAttribute('rowspan')) {
-                        Ext.get(last_event_cell).setHeight((last_event_cell.getAttribute('rowspan')-1) * (eventRowHeight+2));
-                    } else if (last_event_cell !== undefined && !last_event_cell.hasAttribute('rowspan')) {
-                        if (Ext.get(last_event_cell).child('div') == null) {
-                            Ext.get(last_event_cell).update('');
-                        }
-                        if (Ext.get(last_event_cell).up('tr').up('table').select('tr').elements.length > 2) {
-                            Ext.get(last_event_cell).setHeight(eventRowHeight/4);
-                        } else {
-                            Ext.get(last_event_cell).setHeight(eventRowHeight+3);
-                        }
-                        Ext.get(last_event_cell).parent().show();
-                    }
+                last_event_cell = (last_event_cell !== undefined ? last_event_cell.elements[0] : undefined);
+                if (last_event_cell === undefined) return;
+                if (last_event_cell.hasAttribute('rowspan')) {
+                    Ext.get(last_event_cell).setHeight((last_event_cell.getAttribute('rowspan') * me.eventRowHeight)+2);
+                } else {
+                    Ext.get(last_event_cell).setHeight(me.eventRowHeight+1);
+                    Ext.get(last_event_cell).parent().show();
                 }
             });
-        }
        this.fireEvent('eventsrendered', this);
     },
 	
@@ -220,9 +204,9 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
             h = tbl.getHeight();
         // These should be auto-height, but since that does not work reliably
         // across browser / doc type, we have to size them manually
-        this.el.setHeight(h + 2);
-        this.el.down('.ext-cal-hd-ad-inner').setHeight(h + 1);
-        this.el.down('.ext-cal-bg-tbl').setHeight(h + 1);
+        this.el.setHeight(h + 7);
+        this.el.down('.ext-cal-hd-ad-inner').setHeight(h + 5);
+        this.el.down('.ext-cal-bg-tbl').setHeight(h + 5);
     },
 
     //private
@@ -230,20 +214,20 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
      * Called from Extensible.calendar.dd.SchedulerHDropZone it triggers the private method that handles the behaviour
      * when the event is dropped in the view
      * @param rec Event data record
-     * @param calIdx Index of the current calendar item in the calendar store - from the current cell under the mouse's pointer
+     * @param calendarId Id of the current calendar item in the calendar store - from the current cell under the mouse's pointer
      * @param mode
      */
-    onEventDrop: function(rec, calIdx, mode) {
+    onEventDrop: function(rec, calendarId, mode) {
         // instead of moveEvent from the abstract class we reffer to our own method
-        this[(mode || 'move') + 'Event'](rec, calIdx);
+        this[(mode || 'move') + 'Event'](rec, calendarId);
     },
 
     /**
      * Called from Extensible.calendar.dd.SchedulerHDropZone if the drag/drop is made on an empty calendar cell.
-     * @param calIdx Index of the current calendar item in the calendar store - from the current cell under the mouse's pointer
+     * @param calendarId Id of the current calendar item in the calendar store - from the current cell under the mouse's pointer
      * @param onComplete callback function that can be passed to be executed at the end of this method
      */
-	onCalendarEndDrag: function(calIdx, onComplete) {
+	onCalendarEndDrag: function(calendarId, onComplete) {
 		// set this flag for other event handlers that might conflict while we're waiting
 		this.dragPending = true;
 		var boundOnComplete = Ext.bind(this.onCalendarEndDragComplete, this, [onComplete]);
@@ -254,7 +238,7 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
             rec.data[M.StartDate.name] = this.startDate;
             rec.data[M.EndDate.name] = this.endDate;
             rec.data[M.IsAllDay.name] = true;
-            rec.data[M.CalendarId.name] = this.calendarStore.data.items[calIdx].data.CalendarId;
+            rec.data[M.CalendarId.name] = calendarId;
 
 		if (this.fireEvent('rangeselect', this, rec, boundOnComplete) !== false) {
 			this.showEventEditor(rec, null);
@@ -274,34 +258,33 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
     /**
      * Create a copy of the event with new CalendarId preserving the other data of the event.
      * @param {Object} rec The original event {@link Extensible.calendar.data.EventModel record}
-     * @param {Object} calIdx The new calendar index that represents the position of the calendar record in the calendar
+     * @param {Object} calendarId The new calendar Id that represents the position of the calendar record in the calendar
      * store from which we take the CalendarId
      */
-    copyEvent: function(rec, calIdx) {
-        this.modifyEvent(rec, calIdx, 'copy');
+    copyEvent: function(rec, calendarId) {
+        this.modifyEvent(rec, calendarId, 'copy');
     },
 
     /**
      * Move the event by setting a new CalendarId preserving the other data of the event.
      * @param {Object} rec The original event {@link Extensible.calendar.data.EventModel record}
-     * @param {Object} calIdx The new calendar index that represents the position of the calendar record in the calendar
+     * @param {Object} calendarId The new calendar Id that represents the position of the calendar record in the calendar
      * store from which we take the CalendarId
      */
-    moveEvent: function(rec, calIdx) {
-        this.modifyEvent(rec, calIdx, 'move');
+    moveEvent: function(rec, calendarId) {
+        this.modifyEvent(rec, calendarId, 'move');
     },
 
     /**
      * This method is our custom implementation of the shiftEvent method from the AbstractCalendar class. It is used
      * for moving/copying the event on another calendar.
      * @param rec Current event record
-     * @param calIdx Index of the current calendar item in the calendar store - from the current cell under the mouse's pointer
+     * @param calendarId Id of the current calendar item in the calendar store - from the current cell under the mouse's pointer
      * @param moveOrCopy
      */
-    modifyEvent: function(rec, calIdx, moveOrCopy) {
+    modifyEvent: function(rec, calendarId, moveOrCopy) {
         var me = this,
-            newRec,
-            calendarId = me.calendarStore.data.items[calIdx].data.CalendarId;
+            newRec;
 
         if (moveOrCopy === 'move') {
             if (rec.data.CalendarId === calendarId) {
@@ -312,36 +295,7 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
         } else {
             newRec = rec.clone();
         }
-
-        if (me.fireEvent('beforeevent' + moveOrCopy, me, newRec, calendarId) !== false) {
-            if (newRec.isRecurring()) {
-                //if (me.recurrenceOptions.editSingleOnDrag) {
-                me.onRecurrenceEditModeSelected('single', newRec, calendarId, moveOrCopy);
-                //}
-                // else {
-                // Extensible.form.recurrence.RangeEditWindow.prompt({
-                // callback: Ext.bind(me.onRecurrenceEditModeSelected, me, [newRec, newStartDate, moveOrCopy], true),
-                // editModes: ['single', 'future'],
-                // scope: me
-                // });
-                // }
-            } else {
-               me.doModifyEvent(newRec, calendarId, moveOrCopy);
-            }
-        }
-    },
-
-    onRecurrenceEditModeSelected: function(editMode, rec, calId, moveOrCopy) {
-        var EventMappings = Extensible.calendar.data.EventMappings;
-        if (editMode) {
-            if (moveOrCopy === 'copy') {
-                rec.clearRecurrence();
-            }
-            rec.data[EventMappings.REditMode.name] = editMode;
-            rec.data[EventMappings.RInstanceStartDate.name] = rec.getStartDate();
-            this.doModifyEvent(rec, calId, moveOrCopy);
-        }
-        // else user canceled
+        me.doModifyEvent(newRec, calendarId, moveOrCopy);
     },
 
     /**
@@ -351,17 +305,17 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
      * @param calId
      * @param moveOrCopy
      */
-    doModifyEvent: function(rec, calId, moveOrCopy) {
+    doModifyEvent: function(rec, calendarId, moveOrCopy) {
         var EventMappings = Extensible.calendar.data.EventMappings,
             updateData = {};
 
-        updateData[EventMappings.CalendarId.name] = calId;
+        updateData[EventMappings.CalendarId.name] = calendarId;
         rec.set(updateData);
         if (rec.phantom) {
             this.store.add(rec);
         }
         this.save();
-        this.ownerCt.fireEvent('event' + moveOrCopy + 'tocalendar', this, rec);
+      //  this.ownerCt.fireEvent('event' + moveOrCopy + 'tocalendar', this, rec);
     },
 
     /**
@@ -400,10 +354,10 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
         var dt = Ext.Date.format(this.startDate,'Ymd');
         if (el) {
             idxCal = el.id.split('calendar')[2];
-            if (idxCal===undefined && Ext.get(el).up('table').up('td')!==null){
+            if (idxCal === undefined && Ext.get(el).up('table').up('td') !== null){
                 idxCal = Ext.get(el).up('table').up('td').id.split('calendar')[2];
             }
-            idxCal = idxCal === undefined ? -1: idxCal.split('-')[1];
+            idxCal = (idxCal === undefined ? -1: idxCal.split('-')[1]);
             if (idxCal === -1 || el == null) return false; //clicked outside usable area....
 
             if (el.id && el.id.indexOf(this.dayElIdDelimiter) > -1) {
@@ -411,7 +365,7 @@ Ext.define('Extensible.calendar.view.SchedulerHeader', {
                 dt = parts[parts.length - 1];
 
                 this.onDayClick(Ext.Date.parseDate(dt, 'Ymd'), true, Ext.get(this.getDayId(dt, true)),
-                    this.calendarStore.data.items[idxCal]);
+                    this.calendarStore.findRecord('CalendarId',idxCal));
                 return;
             }
         }
